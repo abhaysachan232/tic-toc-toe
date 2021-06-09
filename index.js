0b5350796d3b52ae3ea8eee90b55788c7f92a083
abhaysachan232 <abhaysachan232@gmail.com>
2021-06-09 19:05:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ selectOBtn = selectBox.querySelector('.playerO'),
 playBoard = document.querySelector('.play-board'),
 AllBox = document.querySelectorAll('section span'),
 players = document.querySelector('.players');
+resultbox = document.querySelector('.result-box'),
+wontext = resultbox.querySelector('.won-text'),
+replaybtn = resultbox.querySelector('button');
 
 window.onload = ()=>{
     for (let i = 0; i < AllBox.length; i++) {
@@ -13,13 +16,14 @@ window.onload = ()=>{
     selectXBtn.onclick=()=>{
         selectBox.classList.add('hide'); 
         playBoard.classList.add('show');
-        players.setAttribute('class','players active player');
+        
 
 
     }
     selectOBtn.onclick=()=>{
         selectBox.classList.add('hide'); 
         playBoard.classList.add('show');
+        players.setAttribute('class','players active player');
 
 
     }
@@ -39,10 +43,10 @@ if(players.classList.contains('player')){
 
 }else{
     element.innerHTML = `<i class="${playerXIcon}"></i>`
-    players.classList.add('active');
+    players.classList.add('active'); 
     element.setAttribute('id',playersign);
 }
-
+selectwinner()
 element.style.pointerEvents= 'none';
 let randomDelaytime = ((Math.random()*1000 )+ 200).toFixed();
 setTimeout(()=>{bot(runbot)},randomDelaytime)
@@ -85,18 +89,35 @@ function bot(){
 }
 
 }
-
 function getClass(idname){
-    return document.querySelector(".box" + idname).id;
+    return document.querySelector('.box' + idname).id;
 }
-function checkthreeclasses(val1, val2, val3, sign){
-    if(getClass(val1)== sign && getClass(val2)== sign && getClass(val3)==sign){
+
+function checkclass(val1,val2,val3,sign){
+    if (getClass(val1) == sign && getClass(val2) == sign && getClass(val3) == sign){
         return true;
-    } 
+    }
 }
 function selectwinner(){
-    if(checkthreeclasses(1, 2, 3, playersign) || checkthreeclasses(4, 5, 6, playersign) || checkthreeclasses(6, 7, 8, playersign) || checkthreeclasses(1, 4, 7, playersign)|| checkthreeclasses(2, 5, 8, playersign) || checkthreeclasses(3, 6, 9, playersign) || checkthreeclasses(1, 5, 9, playersign) || checkthreeclasses(3, 5, 7, playersign))
-     console.log(playersign + " " + "is the winner");
-     runbot= false;
-     bot(runbot);
-}
+    if (checkclass(1,2,3,playersign)|| checkclass(4,5,6,playersign)||checkclass(7,8,9,playersign)||checkclass(1,4,7,playersign)||checkclass(2,5,8,playersign)||checkclass(3,6,9,playersign)||checkclass(1,5,9,playersign)||checkclass(3,5,7,playersign)){
+        runbot= false;
+        bot(runbot);
+         
+            playBoard.classList.remove('show');
+            resultbox.classList.add('show');        
+        wontext.innerHTML =`player <p>${playersign}</p> won the match`
+    }else{
+        if(getClass(1)!=="" && getClass(2)!==""&& getClass(3)!=="" && getClass(4)!=="" && getClass(5)!=="" && getClass(6)!=="" && getClass(7)!=="" && getClass(8)!=="" && getClass(9)!=="") {
+            runbot= false;
+            bot(runbot);
+            setTimeout(()=>{ 
+                playBoard.classList.remove('show');
+                resultbox.classList.add('show');
+            },700);
+            wontext.textContent = `Match has been drawn`;
+        }
+        }
+    }
+    replaybtn.onclick = ()=>{
+        window.location.reload();
+    }
